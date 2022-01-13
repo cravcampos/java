cbb8373768e3a2ca9d16f412e5a6ddf1fa390dfa
Victor <cravcampos@gmail.com>
2022-01-13 00:47:31 -0500

    Creación de nuestra primera app en java

---
+++ src/com/java/seccion01_primeraapp/PrimeraApp.java
@@ -0,0 +1,7 @@
+package com.java.seccion01_primeraapp;
+
+public class PrimeraApp {
+    public static void main(String[] args) {
+        System.out.println("Victor Alfonso Campos Rodriguez");
+    }
+}
