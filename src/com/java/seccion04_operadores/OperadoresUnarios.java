03a21a55cb69c75c7e46307222d23c5658a9fa07
Victor <cravcampos@gmail.com>
2022-01-15 23:01:47 -0500

    Ejemplo de uso de operadores unarios + y -

---
+++ src/com/java/seccion04_operadores/OperadoresUnarios.java
@@ -0,0 +1,26 @@
+package com.java.seccion04_operadores;
+
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+
+        //manejo de números positivos y negativos
+        int i = -5;
+        int j = +i;// j = (1) * i = -5
+        System.out.println("j = " + j);
+
+        int k = -i; // k = (-1) * i = 5
+        System.out.println("k = " + k);
+
+        i = 6;
+        j = +i;
+        System.out.println("j = " + j);
+
+        k = -i;
+        System.out.println("k = " + k);
+
+
+
+
+
+    }
+}
