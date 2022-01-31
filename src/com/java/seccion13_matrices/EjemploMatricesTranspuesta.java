9c2f2de676250d32b06f5d93cd0bad4e12a189e7
Victor <cravcampos@gmail.com>
2022-01-30 22:47:12 -0500

    Ejemplo de pasar una matriz a su transpuesta

---
+++ src/com/java/seccion13_matrices/EjemploMatricesTranspuesta.java
@@ -0,0 +1,41 @@
+package com.java.seccion13_matrices;
+
+public class EjemploMatricesTranspuesta {
+    public static void main(String[] args) {
+
+        int[][] matriz = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        int aux;
+
+        // Mostramos la matriz original
+        System.out.println("Matriz Original");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        // Transponemos la matriz
+        for (int i = 1; i < matriz.length; i++) {
+            for (int j = 0; j < i; j++) {
+                aux = matriz[i][j];
+                matriz[i][j] = matriz[j][i];
+                matriz[j][i] = aux;
+            }
+        }
+
+        // Mostramos la matriz transpuesta
+        System.out.println("Matriz Transpuesta");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
